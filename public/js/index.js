f61bcf23bffed394efef9356a73e6f8edd992dfb
unknown <vahedi.ce@gmail.com>
2018-11-18 03:14:26 +0330

    Adding chat form with jQuery

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io()
 socket.on('connect',()=>{
     console.log('New User Connect !')
 //For Create Message For Server
-    socket.emit('createMessage',{
-        from:"Mohammad",
-        text:"Hello Socket"
-    })
+    // socket.emit('createMessage',{
+    //     from:"Mohammad",
+    //     text:"Hello Socket"
+    // })
 })
 socket.on('disconnect',()=>{
     console.log('Disconnect from Server !')
@@ -13,4 +13,17 @@ socket.on('disconnect',()=>{
 //For Create Message to Server
 socket.on('newMessage',(message)=>{
     console.log('newMessage',message)
+
+    var li = jQuery('<li></li>')
+    li.text(`${message.from} : ${message.text}`)
+    jQuery('#messages').append(li)
+})
+
+jQuery('#messageForm').on('submit',function(e){
+    e.preventDefault()
+
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    })
 })
