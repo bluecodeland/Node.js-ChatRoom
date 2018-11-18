6021613c4577de0295f1323667f36af996cc5d88
unknown <vahedi.ce@gmail.com>
2018-11-18 18:25:07 +0330

    Added Style to the ChatRoom

--- public/js/index.js
+++ public/js/index.js
@@ -25,5 +25,35 @@ jQuery('#messageForm').on('submit',function(e){
     socket.emit('createMessage',{
         from:'User',
         text:jQuery('[name=message]').val()
+    }, function(){
+        jQuery('[name=message]').val('')
     })
+})
+
+var locationButton = jQuery('#sendLocation')
+locationButton.on('click',function(){
+    if(!navigator.geolocation){
+        return alert('Geo Location not On')
+    }
+    locationButton.attr('disabled','disabled').text('Sending Location...')
+    navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send Location')
+        socket.emit('createLocationMessage',{
+            latitude:position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function(){
+        locationButton.removeAttr('disabled').text('Send Location')
+        alert('Unable to fetch Location !')
+    })
+})
+
+socket.on('newLocationMessage',function(message){
+    var li =jQuery('<li></li>')
+    var a = jQuery('<a target="_blank">My Current Position </a>')
+
+    li.text(`${message.from}: `)
+    a.attr('href',message.url)
+    li.append(a)
+    jQuery('#messages').append(li)
 })
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const socketIO = require('socket.io')
 const http = require('http')
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.port || 3000
-
+const {generateMessage,generateLocationMessage} = require('../public/js/messages')
 var app = express()
 var server = http.createServer(app)
 var io = socketIO(server)
@@ -13,31 +13,19 @@ var io = socketIO(server)
 io.on('connection',(socket)=>{
     console.log('New User Connect')
 
-    socket.emit('newMessage',{
-        from:"Admin",
-        text:"Welcome to the ChatRoom",
-        createdAt:new Date().getTime()
-    })
+    socket.emit('newMessage',generateMessage('Admin','Welcome to the ChatRoom'))
 
-    socket.broadcast.emit('newMessage',{
-        from:"ChatRoom",
-        text:"New User Joind",
-        createdAt:new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage',generateMessage('ChatRoom','New User Joind'))
+    
 //For Create Message to Client
-    socket.on('createMessage',(message)=>{
+    socket.on('createMessage',(message,callback)=>{
         console.log('Create Message',message)
-        io.emit('newMessage',{
-            from:message.from,
-            text:message.text,
-            createdAt:new Date().getTime()
-        })
-
-        // socket.broadcast.emit('newMessage',{
-        //     from:message.from,
-        //     text:message.text,
-        //     createdAt:new Date().getTime()
-        // })
+        io.emit('newMessage',generateMessage(message.from,message.text))
+        callback()
+    })
+//For Create Location Message        
+    socket.on('createLocationMessage',(coords)=>{
+        io.emit('newLocationMessage',generateLocationMessage('Admin', coords.latitude,coords.longitude))
     })
 //Disconnect Socket    
     socket.on('disconnect',()=>{
@@ -50,4 +38,3 @@ app.use(express.static(publicPath))
 server.listen(port,()=>{
     console.log(`Server is Running on ${port}...!`)
 })
-
